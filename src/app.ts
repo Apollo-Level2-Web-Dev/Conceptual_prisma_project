98c259fc851383ccfb7346a36d555a3d6b2631d4
MD shafayat Islam <shafayat.ms@gmail.com>
2024-11-10 00:28:54 +0600

    all done

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import { authRoutes } from "./app/modules/auth/auth.route";
+import { bookRoutes } from "./app/modules/Book/book.route";
+import { authorRoutes } from "./app/modules/author/author.route";
+import { categoryRoutes } from "./app/modules/category/category.route";
+import authMiddleware from "./app/middleware/auth";
+
+const app: Application = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/author", authorRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/books", authMiddleware, bookRoutes);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    message: "The server is running",
+  });
+});
+
+export default app;
